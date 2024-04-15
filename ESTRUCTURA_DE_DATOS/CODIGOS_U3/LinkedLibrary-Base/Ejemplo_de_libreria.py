589431130383f658083bc45c1f0387e9548cede8
Angela Chable <148722779+AngelaChable@users.noreply.github.com>
2024-04-15 15:24:27 -0600

    Add files via upload

---
+++ ESTRUCTURA_DE_DATOS/CODIGOS_U3/LinkedLibrary-Base/Ejemplo_de_libreria.py
@@ -0,0 +1,12 @@
+import Library as ae
+
+listaEnlanzada= ae.linked_list ()
+
+listaEnlanzada.add_at_front (1)
+listaEnlanzada.add_at_end (2)
+listaEnlanzada.add_at_front (3)
+listaEnlanzada.add_at_end (4)
+listaEnlanzada.add_at_front (5)
+listaEnlanzada.add_at_end (6)
+listaEnlanzada.delete_node(1)
+listaEnlanzada.print_list ()
---
+++ ESTRUCTURA_DE_DATOS/CODIGOS_U3/LinkedLibrary-Base/Setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Library',
+    version= '0.0.1',
+    packages= find_packages(),
+    py_modules= ["Library"],
+    )
